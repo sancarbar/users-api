fd7c66d3b122f152c1ed2db74437d5443f352e27
sancarbar <sancarbar@gmail.com>
2022-03-17 10:59:50 -0500

    1. Initial commit base project from Spring Initilzr.

---
+++ src/test/java/org/adaschool/user/UserApplicationTests.java
@@ -0,0 +1,15 @@
+package org.adaschool.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests
+{
+
+	@Test
+	void contextLoads()
+	{
+	}
+
+}
